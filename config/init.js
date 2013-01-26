04b9c5908df0cecdfe165563736793009dfcd2e9
Thorsten Lorenz <thlorenz@gmx.de>
2013-01-25 22:27:52 -0500

    fixing map fn order

--- config/init.js
+++ config/init.js
@@ -4,12 +4,13 @@ var resolve = require('./resolve')
   , log = require('../lib/log');
 
 module.exports = function (repl, vim, cb) {
+  console.log(vim);
   resolve(function (config) {
     if (config.map) {
       if (typeof config.map !== 'function')
         log.error('Found "map" in config, but it is a [%s]. It needs to be a function (ignoring for now).', typeof config.map);
       else 
-        config.map(vim.map.insert, vim.map.normal);
+        config.map(vim.map.normal, vim.map.insert);
     }
     cb();
   });
