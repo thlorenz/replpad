aa862416bd4f0750ec866d80e5726e6119b0912c
Thorsten Lorenz <thlorenz@gmx.de>
2014-04-20 15:10:27 -0400

    working version of compiling ES6 and re-evaling when initial eval fails

--- repreprep.js
+++ repreprep.js
@@ -1,5 +1,4 @@
 'use strict';
-
 var repl            =  require('repl')
   , path            =  require('path')
   , cardinal        =  require('cardinal')
@@ -19,6 +18,59 @@ var repl            =  require('repl')
   , stdin           =  process.stdin
   , stdout          =  process.stdout
   ;
+
+var vm     = require('vm')
+  , log    = require('./lib/log')
+  , compileES6 = require('es6ify/compile')
+
+function insp(obj, depth) {
+  return require('util').inspect(obj, false, depth || 5, true);
+}
+var traceurOverrides = { sourceMap: false, blockBinding: true };
+
+function builtinEval(useGlobal, code, context, file, cb) {
+  var err, result;
+  try {
+    if (useGlobal) {
+      result = vm.runInThisContext(code, file);
+    } else {
+      result = vm.runInContext(code, context, file);
+    }
+  } catch (e) {
+    err = e;
+  }
+  if (err && process.domain) {
+    process.domain.emit('error', err);
+    process.domain.exit();
+  }
+  else {
+    cb(err, result);
+  }
+}
+
+function enhancedEval(useGlobal, code, context, file, cb) {
+  builtinEval(useGlobal, code, context, file, function (err, result) {
+    if (!err) return cb(null, result);
+
+    var noparen = code.slice(1, -1);
+
+    // encountered an error so let's hope it was ES6 code and try to compile it
+    var compiled = compileES6(file, noparen, traceurOverrides)
+    if (compiled.errors && compiled.errors.length)
+      return cb(new Error('Unable to compile ES6\t\n' + insp(compiled.errors)));
+
+    // remove 'use strict'; and `var _moduleName = "repl"` lines
+    var relevantLines = compiled.source ? compiled.source.split('\n').slice(2) : [];
+
+    // wrapping inside {} instead of () works better especially if there are var or let statements
+    var es5 = '{' + relevantLines.join('\n') + '}';
+    log.silly('Running\t\n', es5); 
+
+    // ES6 compiled fine, so lets try again and hope for the best
+    builtinEval(useGlobal, es5, context, file, cb);
+  })
+}
+
 function createRepl(opts) {
   var r = repl.start(opts);
   log.repl = r;
@@ -79,6 +131,10 @@ module.exports = function repreprep(root, opts) {
   // override prompt with the one given in opts AFTER config was initialized
   config.prompt = opts.prompt || config.prompt || 'pad > ';
   replOpts.prompt = config.prompt;
+
+
+  /*jshint evil: true */
+  replOpts.eval = enhancedEval.bind(null, replOpts.useGlobal);
 
   if (!root) {
     log.print('Watching no files since no path was specified.');
