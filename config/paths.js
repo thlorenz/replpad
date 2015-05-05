dce81967735100d9d50f9cbda2e98b0a62b89673
Mark Yen <mark@uber.com>
2015-05-05 14:27:21 -0700

    Warn user if home dir not set

--- config/paths.js
+++ config/paths.js
@@ -1,14 +1,15 @@
 'use strict';
 
 var path       =  require('path')
+  , os         =  require('os')
   , home       =  process.env.HOME || process.env.USERPROFILE
-  , root       =  path.join(home, '.config', 'replpad')
-  , cachesRoot =  path.join(root, 'cache')
+  , root       =  home && path.join(home, '.config', 'replpad')
+  , cachesRoot =  path.join(root || os.tmpdir(), 'cache')
   , caches     =  path.join(cachesRoot, process.version)
   ;
 
 module.exports = {
     root       :  root
   , caches     :  caches
-  , configFile :  path.join(root, 'config.js')
+  , configFile :  root && path.join(root, 'config.js')
 };
