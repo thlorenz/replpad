990c520f7a649399e7f6e78ae3b2e4e3f6857d66
David Krmpotic <david.krmpotic@gmail.com>
2016-03-22 00:31:47 +0100

    bugfix nodejs.org has to be called via https

--- test/lib/dox/core/retrieve.js
+++ test/lib/dox/core/retrieve.js
@@ -4,12 +4,12 @@
 var test     =  require('tap').test
   , retrieve =  require('../../../../lib/dox/core/retrieve')
   , format   =  require('util').format
-  , rootUrl  =  format('http://nodejs.org/docs/%s/api', process.version)
+  , rootUrl  =  format('https://nodejs.org/docs/%s/api', process.version)
 
 
 test('\nwhen I retrieve fs', function (t) {
   retrieve(rootUrl + '/fs.json', 'fs', function (err, doc) {
-    t.equal(err, null, 'returns no error')    
+    t.equal(err, null, 'returns no error')
     t.equal(doc.modules[0].name, 'fs', 'returns doc for file module')
     t.end()
   })
@@ -17,7 +17,7 @@ test('\nwhen I retrieve fs', function (t) {
 
 test('\nwhen I retrieve path', function (t) {
   retrieve(rootUrl + '/path.json', 'path', function (err, doc) {
-    t.notOk(err, 'returns no error')    
+    t.notOk(err, 'returns no error')
     t.equal(doc.modules[0].name, 'path', 'returns doc for path module')
     t.end()
   })
@@ -25,7 +25,7 @@ test('\nwhen I retrieve path', function (t) {
 
 test('\nwhen I retrieve nonexistingmodule', function (t) {
   retrieve(rootUrl + '/nonexistingmodule.json', 'nonexistingmodule', function (err, doc) {
-    t.ok(err, 'returns error')    
+    t.ok(err, 'returns error')
     t.similar(err.message, /.+not found./, 'err indicates that the documentation was not found')
     t.end()
   })
